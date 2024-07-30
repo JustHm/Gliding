cb8a2fcffe7ae3cb6a50c524dfc3cbc4d3606d8a
JustHm <taggma12@gmail.com>
2024-07-30 17:20:25 +0900

    HomeViewModel 이벤트 구조 추가

--- Gliding/HomeScene/HomeViewModel.swift
+++ Gliding/HomeScene/HomeViewModel.swift
@@ -7,18 +7,45 @@
 
 import Foundation
 import RxSwift
+import RxRelay
+import RxCocoa
 
 
 
 final class HomeViewModel: ViewModel {
+    private let todayRelay: BehaviorRelay<Int>
+    private let poolListRelay: BehaviorRelay<Int>
+    private let tipListRelay: BehaviorRelay<Int>
+    private let trainingListRelay: BehaviorRelay<Int>
+    
+    private let urlSession: URLSession
+    private let healthData: HealthData
+    
     struct Input {
-        
+        let refresh: PublishRelay<Void>
+        let trainingDetail: PublishRelay<Int> // Int -> ID
+        let poolListDetail: PublishRelay<String> // road address
+        let tipDetail: PublishRelay<String> //
     }
     struct Output {
+        let today: Driver<Int> // 오늘 운동 현황 데이터
+        let swimTip: Driver<Int> // 수영 팁 아티클 리스트
+        let trainingList: Driver<Int> // 훈련표 리스트
+        let poolList: Driver<Int> // 수영장 정보 리스트
         
     }
     
+    init(
+        healthData: HealthData = HealthData.instance,
+        urlSession: URLSession = URLSession.shared
+    ) {
+        self.healthData = healthData
+        self.urlSession = urlSession
+    }
+    
+    
     func transform(input: Input) -> Output {
+        
         
     }
     
