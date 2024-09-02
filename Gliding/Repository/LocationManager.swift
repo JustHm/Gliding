f4c2ae5790ff874bd05e6fe086695f8040222156
JustHm <taggma12@gmail.com>
2024-09-02 19:36:45 +0900

    location Manager 권한 확인, 위치 정보 업데이트 추가

--- Gliding/Repository/LocationManager.swift
+++ Gliding/Repository/LocationManager.swift
@@ -3,12 +3,41 @@
 //  Gliding
 //
 //  Created by 안정흠 on 8/27/24.
-//
+//  https://co-dong.tistory.com/73
 
 import CoreLocation
 
-final class LocationManager {
+final class LocationManager: NSObject {
     // 위치 정보 권한 획득
     // 위치 정보에 따른 도로명 주소 변환
     // 위치 정보 리프레시
+    private var locationManager = CLLocationManager()
+    var location: CLLocation?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = .greatestFiniteMagnitude
+        if checkAuthorizationStatus() { locationManager.startUpdatingLocation() }
+    }
+    
+    func checkAuthorizationStatus() -> Bool {
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return true
+        case .denied: // 사용자가 권한 삭제한 경우
+            return false
+        case .notDetermined, .restricted:
+            locationManager.requestWhenInUseAuthorization()
+            return checkAuthorizationStatus()
+        @unknown default:
+            return false
+        }
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last
+    }
 }
