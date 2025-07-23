78a8f1b894b115e4503691651f627d1c27facec8
JungHm <taggma12@gmail.com>
2025-07-23 16:59:42 +0900

    Initial Commit

---
+++ GlidingTests/GlidingTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlidingTests.swift
+//  GlidingTests
+//
+//  Created by 안정흠 on 7/23/25.
+//
+
+import Testing
+@testable import Gliding
+
+struct GlidingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
