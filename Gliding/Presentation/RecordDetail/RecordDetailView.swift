1ed64ca9fbecde6ba31248b0c2c6d88e1cc90944
JungHm <taggma12@gmail.com>
2025-09-02 02:13:56 +0900

    Feat: 과거 기록 상세 화면 추가

---
+++ Gliding/Presentation/RecordDetail/RecordDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordDetailView.swift
+//  Gliding
+//
+//  Created by 안정흠 on 9/1/25.
+//
+
+import SwiftUI
+
+struct RecordDetailView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    RecordDetailView()
+}
